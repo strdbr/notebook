0b575e86163b10ec3da8c6bdaac31b0cb5e00552
Emmanuel Estrada <e.estrada1917@gmail.com>
2021-10-13 18:48:25 -0500

    First draft

---
+++ src/components/Content.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Content = () =>(
+   <div className='backColor'>
+    <h2>¿Qué es React?</h2>
+    <p>React es un framework que se basa en uso de componentes</p>
+   </div>
+);
+
+export default Content;
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Header = () =>(
+   <header className='head'>
+       <h1 className='mainTitle'>Notebook</h1>
+   </header>
+);
+
+export default Header;
